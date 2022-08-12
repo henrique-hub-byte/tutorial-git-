043caa09d97bd2f2103cf544820093401d79a7bc
Nadine Gomes Lau da Silva <nadine.lau@flieger.software>
2022-08-12 10:50:54 -0300

    função imposto de renda

--- php/funcoes.php
+++ php/funcoes.php
@@ -31,6 +31,29 @@
         }
 
         criadorDaFunc("luis henrique");
+
+        echo "<hr>Exercicio<hr>";
+
+        function CalcularImposto($salario){
+            $imposto = 0;
+
+            if($salario <= 1903.98){
+                return $imposto;
+            }else if($salario >= 1903.9 && $salario <= 2826.65){
+                $imposto = ($salario * 7.5) / 100;
+                return $imposto;
+            }else if($salario >= 2826.66 && $salario <= 3751.05){
+                return $imposto = ($salario * 15) / 100;
+            }else if ($salario >= 3752.06 && $salario <= 4664.08){
+                return $imposto = ($salario * 22.5) / 100;
+            }else if($salario >= 4.665){
+                return $imposto = ($salario * 27.5) / 100;
+            }
+
+            return $imposto;
+        }
+
+        echo CalcularImposto(2000.98);
     ?>
 </body>
 </html>
