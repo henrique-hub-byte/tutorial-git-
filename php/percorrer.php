6c4920c3bd106431762174a2d67b6eb046cfb8d4
Nadine Gomes Lau da Silva <nadine.lau@flieger.software>
2022-08-12 08:39:29 -0300

    funções em  php

---
+++ php/percorrer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Percorrer um array</title>
+</head>
+<body>
+    <?php
+        $registro = array(
+            array('titulo' => 'titulo noticia 1', 'conteudo' => 'conteudo noticia 1'),
+            array('titulo' => 'titulo noticia 2', 'conteudo' => 'conteudo noticia 2' ),
+            array('titulo' => 'titulo noticia 3', 'conteudo' => 'conteudo noticia 3' ));
+            /* $array = []; *3
+        $indice = 03
+        while($indice < 3){
+            echo $registro[$indice];
+            echo "<hr>";
+            $indice++;
+        }*/
+    ?>
+</body>
+</html>
